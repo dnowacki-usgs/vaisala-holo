ed9a3d078cd2dfe1ca6733bb145062f409c979a1
Dan Nowacki <dnowacki@usgs.gov>
2021-04-29 11:10:58 -0700

    variable typo

--- vaisala-holo.py
+++ vaisala-holo.py
@@ -58,7 +58,7 @@ deviceid = {  # "gri": "511833",
     "bel": "585918",
     "wts": "511833",  # reusing same device for WTS21 as was used at GRI
 }
-wtstimestart = {"gri": 1579651200, "bel": 1586962800, "wts": 1619713600}
+timestart = {"gri": 1579651200, "bel": 1586962800, "wts": 1619713600}
 latlon = {
     "gri": {"lat": 38.117250, "lon": -122.039833},
     "bel": {"lat": 48.760415, "lon": -122.521977},
