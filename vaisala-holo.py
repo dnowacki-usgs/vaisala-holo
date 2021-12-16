c14e3769f434880d643967b076a9f50a1962175a
Dan Nowacki <dnowacki@usgs.gov>
2021-12-16 09:13:45 -0800

    deal with other bad values

--- vaisala-holo.py
+++ vaisala-holo.py
@@ -126,9 +126,10 @@ if site == "bel":
     dsnew["Dm"] = (dsnew["Dm"] - 15) % 360
 
 # deal with bad signalpct values prior to the merge
-if "signalpct" in dsold:
-    dsold["signalpct"][dsold["signalpct"] < 0] = -9999
-dsnew["signalpct"][dsnew["signalpct"] < 0] = -9999
+for k in ["signalpct", "Dm"]:
+    if k in dsold:
+        dsold[k][dsold[k] < 0] = -9999
+    dsnew[k][dsnew[k] < 0] = -9999
 
 ds = xr.merge([dsold, dsnew])
 
